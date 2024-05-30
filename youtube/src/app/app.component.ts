5b743ac2d02116aeaa6d42d7e1b89215f7f69850
Narahari Hegde <naheg@eg.dk>
2024-05-31 00:36:20 +0530

    updated content page

--- youtube/src/app/app.component.ts
+++ youtube/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContentComponent],
+  imports: [RouterOutlet, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- youtube/src/app/components/content/content.component.ts
+++ youtube/src/app/components/content/content.component.ts
@@ -5,8 +5,35 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.scss'
+  styleUrl: './content.component.scss',
 })
 export class ContentComponent {
+  card1 = async (idd: string) => {
+    console.log(idd);
+    const data = await fetch(`http://localhost:3000/data/${idd}`).then((res) =>
+      res.json()
+    );
+    const { desc, id, pdflink, publishdate, title } = data;
 
+    const author = document.getElementById('author');
+    const descr = document.getElementById('descr');
+    const pd = document.getElementById('pd');
+    const pdf = document.getElementById('pdf');
+
+    if (author) {
+      author.innerText = 'Author name:' + title;
+    }
+    if (descr) {
+      descr.innerText = 'Description: ' + desc;
+    }
+    if (pd) {
+      pd.innerText = 'Publish Date: ' + publishdate;
+    }
+    if (pdf) {
+      pdf.setAttribute('src', pdflink);
+    }
+
+    console.log(data);
+    console.log(desc, id, pdflink, publishdate, title);
+  };
 }
