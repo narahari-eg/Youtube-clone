1e2599188e7da3d644e95f62632e7efb7f944c94
Narahari Hegde <naheg@eg.dk>
2024-05-30 18:12:27 +0530

    updated code

--- youtube/src/app/app.component.ts
+++ youtube/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ContentComponent } from './components/content/content.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'youtube';
